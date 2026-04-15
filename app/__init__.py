bb3a90051f03f94ca1432b1f4fe198e4ad54ce16
GbrOrtiga <gabriel.ortiga@sempreceub.com>
2026-04-14 23:53:15 -0300

    verificação no código

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 
-    # CONFIG CORRETA
+   
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///finance.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
--- test_api.py
+++ test_api.py
@@ -2,20 +2,20 @@ import requests
 
 BASE_URL = "http://127.0.0.1:5000"
 
-# Criar usuário
+
 res = requests.post(f"{BASE_URL}/users/", json={
     "name": "Teste",
     "email": "teste@email.com"
 })
 print("Usuário:", res.json())
 
-# Criar categoria
+
 res = requests.post(f"{BASE_URL}/categories/", json={
     "name": "Teste Categoria"
 })
 print("Categoria:", res.json())
 
-# Criar transação
+
 res = requests.post(f"{BASE_URL}/transactions/", json={
     "user_id": 1,
     "category_id": 1,
